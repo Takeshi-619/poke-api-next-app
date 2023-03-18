c7c3ec3e907f394b1aa4c3544f94479b8dd8a091
Takeshi-619 <take.a.0619@icloud.com>
2023-03-18 18:44:35 +0900

    bug

--- src/pages/api/pokemon.ts
+++ src/pages/api/pokemon.ts
@@ -4,14 +4,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    const { pokemonName } = req.query;
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-    );
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  const { pokemonName } = req.query;
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  );
+  const data = await response.json();
+  res.status(200).json(data);
 }
