e64bce42dc24194ab7bffd6e3fea081b305812b9
Takeshi-619 <take.a.0619@icloud.com>
2023-03-20 12:20:29 +0900

    routes

--- src/pages/api/pokemon.ts
+++ src/pages/api/pokemon.ts
@@ -4,10 +4,7 @@ export default async function handler(req: any, res: any) {
   const errorHandler = (error: string) => {
     console.log(error);
   };
-  try {
-    const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon");
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: errorHandler("missing") });
-  }
+
+  const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon");
+  res.status(200).json(data);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
